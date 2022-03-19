11d59e5dc3b10d655a5dbda869ac19e06a2d2383
Raneen Gaber <78453166+RaneenGaber@users.noreply.github.com>
2022-03-19 19:27:50 +0200

    finsh send emails

--- src/modules/SendMails/index.js
+++ src/modules/SendMails/index.js
@@ -1,12 +1,23 @@
 import {React,useState} from "react";
 import { Form, Col, Row, Button } from "react-bootstrap";
 import Layout from "../../shared/Layout/index";
+import configs from "../../globals/config";
 
 const Email = () => {
 
+  const [subject, setSubject] = useState("");
+  const [from, setFrom] = useState("");
+  const [message, setMessage] = useState("");
+  const [emails, setEmails] = useState("");
+  const onSubjectChange = (e) => setSubject(e.target.value);
+  const onFromChange = (e) => setFrom(e.target.value);
+  const onMessageChange = (e) => setMessage(e.target.value);
+  const onEmailChange = (e) => setEmails(e.target.value);
+
+
   const [validated, setValidated] = useState(false);
 
-  const handleSubmit = (event) => {
+  const handleSubmit1 = (event) => {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -15,15 +26,43 @@ const Email = () => {
 
     setValidated(true);
   };
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = { emails, text:message,subject };
+    console.log(data);
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    };
+    fetch(`${configs.HOST}/send-emails`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((res) => {console.log(res)});
+  };
+
+  
 
   return (
     <Layout>
       <h1>Send Emails</h1>
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form noValidate validated={validated} onSubmit={handleSubmit1}>
         <Row className="mb-3">
-          <Form.Group as={Col} md="6" controlId="validationCustom01">
-            <Form.Label>Emails</Form.Label>
-            <Form.Control required as="textarea" placeholder="Emails" />
+          <Form.Group as={Col} md="6" controlId="validationCustom02">
+            <Form.Label>Subject</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Subject"
+              maxlength="50"
+              minilength="3"
+              value={subject}
+              onChange={onSubjectChange}
+            />
             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           </Form.Group>
           <Form.Group as={Col} md="6" controlId="validationCustom02">
@@ -32,33 +71,42 @@ const Email = () => {
               required
               type="text"
               placeholder="From"
-              maxlength="10"
+              maxlength="25"
+              minilength="3"
+              value={from}
+              onChange={onFromChange}
             />
             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           </Form.Group>
         </Row>
         <Row className="mb-3">
-          <Form.Group as={Col} md="6" controlId="validationCustom02">
-            <Form.Label>Subject</Form.Label>
+          <Form.Group as={Col} md="12" controlId="validationCustom01">
+            <Form.Label>Emails</Form.Label>
             <Form.Control
               required
-              type="text"
-              placeholder="Subject"
-              maxlength="10"
+              as="textarea"
+              placeholder="example1@gmail.com example2@gmail.com ..."
+              value={emails}
+              onChange={onEmailChange}
             />
             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           </Form.Group>
-          <Form.Group as={Col} md="6" controlId="validationCustomUsername">
+          <Form.Group as={Col} md="12" controlId="validationCustomUsername">
             <Form.Label>Message</Form.Label>
             <Form.Control
               as="textarea"
               rows={3}
               required
               placeholder="Write a Message"
+              value={message}
+              onChange={onMessageChange}
             />
           </Form.Group>
         </Row>
-        <Button type="submit">Submit form</Button>
+
+        <Button type="submit" onClick={handleSubmit}>
+          Send
+        </Button>
       </Form>
     </Layout>
   );
