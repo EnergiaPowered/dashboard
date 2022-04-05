0f91f6edd851786cd48182af34395628b855d03e
abdullahalshawafi <abdullahadel.aam@gmail.com>
2022-04-05 03:20:17 +0200

    Bug fix in App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch ,Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import routes from "./globals/routes";
 import "./App.css";
 
 function App() {
-
-   
-       return (
+  return (
     <div className="App">
-      
       <Router>
         <Switch>
-          
-          {
-            routes.map((route ,index) => { 
-             return( <Route key={index} path={route.path} component={route.component} exact/> )
-            })
-          }
+          {routes.map((route, index) => {
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                component={route.component}
+                exact
+              />
+            );
+          })}
         </Switch>
       </Router>
-    
     </div>
-    );
-  }
-    
+  );
+}
+
 export default App;
