10e6f16a124ad5cd6fc5cf9992bde74e31fc117e
AliKhalaf1 <47224980+AliKhalaf1@users.noreply.github.com>
2022-11-29 21:11:08 +0200

    Update config.js

--- src/globals/config.js
+++ src/globals/config.js
@@ -2,7 +2,7 @@ const configs = {
   HOST:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000/api"
-      : "https://handsome-worm-woolens.cyclic.apps/api",
+      : "https://handsome-worm-woolens.cyclic.app/api",
   FRONT_HOST:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3001"
