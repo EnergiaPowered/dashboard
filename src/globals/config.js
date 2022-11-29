54edb2cab971db0be3d0ddc71ed6441981e24cac
AliKhalaf1 <47224980+AliKhalaf1@users.noreply.github.com>
2022-11-29 21:05:06 +0200

    Update config.js

--- src/globals/config.js
+++ src/globals/config.js
@@ -2,7 +2,7 @@ const configs = {
   HOST:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000/api"
-      : "https://ep-api.onrender.com/api",
+      : "https://handsome-worm-woolens.cyclic.apps/api",
   FRONT_HOST:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3001"
